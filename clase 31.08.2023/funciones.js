6ed5d75d227f54431ee7980d1f31d30aa36dbc64
Rayen <rnahuelhual@ing.ucsc.cl>
2023-08-31 11:51:29 -0400

    31-08-2023
    clase tis

---
+++ clase 31.08.2023/funciones.js
@@ -0,0 +1,11 @@
+function cambiarAzulPorId(){
+  document.getElementById("azul").style.backgroundColor="blue";
+};
+
+function verdePorClase(){
+    document.getElementsByClassName("verde")[0].style.backgroundColor="green";
+};
+
+function Reset(){
+    document.getElementById("azul").style.backgroundColor="red";
+};
